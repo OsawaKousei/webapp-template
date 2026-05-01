f58ac0db9a4eb8ceb2ac576269ed32e9a2be8326
OsawaKousei <jwithelis@gmail.com>
2026-05-02 00:06:25 +0900

    起動確認

--- backend/api/app/src/index.ts
+++ backend/api/app/src/index.ts
@@ -7,4 +7,16 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+if (process.env.NODE_ENV === 'development') {
+  // 動的インポートにより、Lambda本番環境に余計なモジュールを巻き込まない
+  import('@hono/node-server').then(({ serve }) => {
+    const port = 3000
+    console.log(`Local dev server is running on port ${port}`)
+    serve({
+      fetch: app.fetch,
+      port
+    })
+  })
+}
+
 export const handler = handle(app)
